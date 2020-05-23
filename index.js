c7815f6c45e854ec56824ed3c094a15b4d91cff1
Choi Jiwon <winning1993@naver.com>
2020-05-23 23:43:31 +0900

    .

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ function handleSubmit(e){
     e.preventDefault();
     console.log(mealInput.value);
     keyword.innerHTML = `"${mealInput.value}" 검색결과`;
-    const x = fetch("http://211.237.50.150:7080/openapi/5bc6c1114d787038aaded73920b3ac1242a0ef147f8d69b4f872188180544c76/json/Grid_20150827000000000226_1/1/1000")
-    console.log(x)
+    axios.get("http://211.237.50.150:7080/openapi/5bc6c1114d787038aaded73920b3ac1242a0ef147f8d69b4f872188180544c76/json/Grid_20150827000000000226_1/1/1000").then(
+        (res) => {
+            console.log(res)
+        }
+    )
+    
     
 }
 
